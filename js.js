5bb7c948b9a2de8d8ce3af0283ddc57f2d22436a
Mikhail Kalonov <l1bredim@ukr.net>
2022-01-24 22:04:25 +0200

    slide code

---
+++ js.js
@@ -0,0 +1,20 @@
+const arrowRight = document.querySelector('.arrow_right');
+const arrowLeft = document.querySelector('.arrow_left');
+const active = document.querySelector('.active');
+const slide = document.querySelectorAll('.slide');
+
+
+
+arrowRight.addEventListener('click', nextSlide);
+function nextSlide() {
+}
+
+
+arrowLeft.addEventListener('click', prevSlide);
+function prevSlide() {
+    
+        console.log(k);
+
+
+}
+  
